4917752c3bd31a1e406b5722ca2eba71257be976
BEAST <imyogeshgaikwad@gmail.com>
2025-12-17 17:18:23 +0000

    Some changes

---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+
+
+function Title(){
+    let name = "Yogesh"
+  return (
+    <div>
+  <h1>I am the Title</h1>
+    <h3>This is my description</h3>
+    <p>Hi this description is for {name.toUpperCase()}</p>
+  </div>
+  )
+}
+
+export default Title
