8bde92fe4dd967bed17c8b271c5ad6eb5a1b7c37
BEAST <imyogeshgaikwad@gmail.com>
2025-12-18 17:54:34 +0000

    some activity

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import "./App.css"
 import Title from "./Title.jsx"
 import Product from "./Product.jsx";  
 import ProductTab from"./ProductTab.jsx"
+import MsgBox from "./msgBox.jsx";
 
 
 function App() {
   return(
+    <>
   <ProductTab/>
+  <MsgBox userName="shradha" textColor="yellow"/>
+  </>
   );
 }
 
